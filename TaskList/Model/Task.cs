261a19666411f6b4738a11c795f86cf47847088d
Vũ Bình Dương <78728283+AiEmStylix@users.noreply.github.com>
2024-04-08 18:40:00 +0700

    first commit

---
+++ TaskList/Model/Task.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskList.Model
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime DueDate { get; set; }
+        public DateTime StartDate { get; set; }
+        public bool IsCompleted { get; set; }
+        public TimeSpan Timer { get; set; }
+
+        public TaskStatus TaskStatus { get; set; }
+        public TaskCategory TaskCategory { get; set; }
+        public TaskImportance TaskImportance { get; set; }
+
+    }
+
+    public enum TaskStatus
+    {
+        /// <summary>
+        /// The task is still is progress
+        /// </summary>
+        InProgress,
+        /// <summary>
+        /// The task has been completed
+        /// </summary>
+        Complete,
+        /// <summary>
+        /// The task is not even started
+        /// </summary>
+        NotStarted,
+        /// <summary>
+        /// The task is late
+        /// </summary>
+        Late,
+        /// <summary>
+        /// The task has been archived
+        /// </summary>
+        Archived,
+        /// <summary>
+        /// The task has been deleted
+        /// </summary>
+        Deleted
+    }
+
+    public enum TaskCategory
+    {
+        Education,
+        Work,
+        Finance,
+        Shopping
+    }
+
+    public enum TaskImportance
+    {
+        Low,
+        Medium, 
+        High,
+        Critical
+    }
+}
